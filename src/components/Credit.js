e570751b50ca390f9e9910c4a8b6fcfd6f81667e
SHAURYA VERMA <96902070+shaurya8688@users.noreply.github.com>
2024-01-16 16:24:39 +0530

    Add files via upload

---
+++ src/components/Credit.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Credit() {
+  return (
+    <div class="credit">
+    <p>
+      Coded by
+      <a 
+        > Shaurya Verma</a
+      >, <a href="https://github.com/shaurya8688/weather-App.git">Open sourced on Github.</a> Hosted on Netlify
+    </p>
+  </div>
+  );
+}
